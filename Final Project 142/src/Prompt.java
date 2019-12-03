96d69cc9a50f6fa52c45ac43a0f8435d94d70a74
Aimee <temp>
2019-12-03 15:01:29 -0800

    menu

---
+++ Final Project 142/src/Prompt.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Prompt {
+
+	public static void main(String[] args) {
+		//Menu: jump to other methods
+		Scanner s = new Scanner(System.in);
+		int choice = 0;
+		
+		System.out.println("Welcome to Grocery Pricing Management!");
+		System.out.println("What would you like to do?");
+		
+		System.out.println("1. Create Item");
+		System.out.println("2. Update Item Values");
+			//FIXME check for item existence
+			//FIXME jump to other menu with list of things to update
+		System.out.println("3. Calculate Profit Margin");
+		System.out.println("4. Update by Margin");
+			//FIXME prompt for new margin
+		System.out.println("5. Total Profit with Items on Sale");
+			//FIXME prompt for numItems and sale 
+		System.out.println("6. Price per Unit");
+			//FIXME prompt for unit size
+	}
+
+}

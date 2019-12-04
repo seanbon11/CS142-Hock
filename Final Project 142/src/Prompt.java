64dbb033d31665dc3754b9e11d9d0407ada8569b
Achamian47 <58159993+Achamian47@users.noreply.github.com>
2019-12-04 15:28:06 -0800

    Added items

--- Final Project 142/src/Prompt.java
+++ Final Project 142/src/Prompt.java
@@ -118,6 +118,27 @@ public class Prompt {
 		Item Rice = new Item(description, cost, regularPrice, salePrice);
 		groceryList.add(Rice);
 		
+		Item ToiletPaper = new Item("Toilet Paper", 0.55, 0.85, 0.85);
+		groceryList.add(ToiletPaper);
+		
+		Item Cashews = new Item("Cashews", 1.68, 2.95, 2.95);
+		groceryList.add(Cashews);
+		
+		Item Tulip = new Item("Tulip", 0.10, 0.25, 0.25);
+		groceryList.add(Tulip);
+		
+		Item Steak = new Item("Steak", 10.55, 15.45, 15.45);
+		groceryList.add(Steak);
+		
+		Item LightBulbs = new Item("Light Bulbs", 3.65, 6.05, 6.05);
+		groceryList.add(LightBulbs);
+		
+		Item SaladDressing = new Item("Salad Dressing", 4.56, 7.81, 7.81);
+		groceryList.add(SaladDressing);
+		
+		Item Floss = new Item("Floss", 0.35, 0.65, 0.65);
+		groceryList.add(Floss);
+		
 		return groceryList;
 			
 	}

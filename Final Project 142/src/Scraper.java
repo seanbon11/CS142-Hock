8b93a06e31d4e6b7390488d7a1fe53ab6f5435db
jpheitman <58095360+jpheitman@users.noreply.github.com>
2019-12-04 15:46:17 -0800

    Adding Scraper class (main method only)

--- Final Project 142/src/Scraper.java
+++ Final Project 142/src/Scraper.java
@@ -1,7 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Scraper {
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner s = new Scanner(new File( "brdatatest.xml" ));
+
+		ArrayList<String> list = new ArrayList<>();
+		while (s.hasNextLine()) {
+			String line = s.nextLine();
+			if (line.startsWith("    <Wh_Item>")) {
+				String number = line.substring(line.indexOf(">")+1, line.indexOf("</"));
+				list.add(number);
+			}
+		}
+		System.out.println(list);
+		System.out.println(list.size());
 		
 	}
 

5c46f802a9a7e6c12cea102e752ee9b011713e89
jpheitman <58095360+jpheitman@users.noreply.github.com>
2019-12-05 14:03:35 -0800

    Updating file locations; more Scanner development

--- Final Project 142/src/Scraper.java
+++ Final Project 142/src/Scraper.java
@@ -19,26 +19,29 @@ public class Scraper {
 	}
 	
 	
-	public static Item loadNewItems(Scanner s) {
+	public static Item loadNewItem(Scanner s) {
 			String line = s.nextLine();
 			String desc = "desc";
 			double cst = 0.0;
 			double retail = 0.0;
 			double tpr = 0.0;
-			if (line.startsWith("	<Description>")) {
-				desc = line.substring(line.indexOf(">")+1, line.indexOf("</"));
-				line = s.nextLine();
-			}
-			if (line.startsWith("	<Cost>")) {
-				cst = Double.valueOf(line.substring(line.indexOf(">")+1, line.indexOf("</")));
-				line = s.nextLine();
-			}
-			if (line.startsWith("	<RegularPrice>")) {
-				retail = Double.valueOf(line.substring(line.indexOf(">")+1, line.indexOf("</")));
-				line = s.nextLine();
-			}
-			if (line.startsWith("	<SalePrice>")) {
-				tpr = Double.valueOf(line.substring(line.indexOf(">")+1, line.indexOf("</")));
+			// Keep going until we see <ROW> tag to see that item started.
+			while (s.hasNextLine() && !line.contains("<ROW>")) line = s.nextLine();
+			if (!s.hasNextLine()) return null;
+			while (!line.contains("</ROW>")) {
+				// Load until we see </ROW> tag to tell us that the item ended.
+				if (line.startsWith("	<Description>")) {
+					desc = line.substring(line.indexOf(">")+1, line.indexOf("</"));
+				}
+				if (line.startsWith("	<Cost>")) {
+					cst = Double.valueOf(line.substring(line.indexOf(">")+1, line.indexOf("</")));
+				}
+				if (line.startsWith("	<RegularPrice>")) {
+					retail = Double.valueOf(line.substring(line.indexOf(">")+1, line.indexOf("</")));
+				}
+				if (line.startsWith("	<SalePrice>")) {
+					tpr = Double.valueOf(line.substring(line.indexOf(">")+1, line.indexOf("</")));
+				}
 				line = s.nextLine();
 			}
 			
@@ -49,10 +52,15 @@ public class Scraper {
 	
 	
 	public static void main(String[] args) throws FileNotFoundException {
-		Scanner t = new Scanner(new File( "sampleitems.xml" ));
-		while(t.hasNextLine()); { 
-			loadNewItems(t);
+		File items = new File("sampleitems.xml");
+		Scanner t = new Scanner(items);
+		ArrayList<Item> itemList = new ArrayList<>();
+		while(t.hasNextLine()) {
+			Item item = loadNewItem(t);
+			if (item == null) break; // done with file
+			itemList.add(item);
 		}
+		System.out.println(itemList);
 	}
 
 }

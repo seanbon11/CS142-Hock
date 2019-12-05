e6f14f9c989c3a0df32a5649af4cc529a785fdc0
Aimee <temp>
2019-12-04 20:16:58 -0800

    some overall edits, part way thru delete item

--- Final Project 142/src/Prompt.java
+++ Final Project 142/src/Prompt.java
@@ -6,80 +6,85 @@ public class Prompt {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		ArrayList<Item> groceryList = fillArrayList();
-		
+
 		String newValue = "";
 
-		int choice = menu(s);
+		while (true) {
+			int choice = menu(s);
 
-		switch (choice) {
-		case 1: // Create New Item
-			Item I = createItem(s);
-			groceryList.add(I);
-			break;
-		case 2: // View Existing Items
-			I = selectItem(groceryList, s);
-			I.printDetails();
-			break;
-		case 3: // Edit an Existing Item
-			I = selectItem(groceryList, s);
-			//newValue = selectNewValue(s);
-			editExistingItem(I, s);
-			break;
-		case 4: //
-			break;
-		case 5: // Delete Item
-			break;
-		case 6: // Quit
-			return;
-		default:
-			System.out.println("/n/nINVALID, try again.");
-			break;
+			switch (choice) {
+			case 1: // Create New Item
+				Item I = createItem(s);
+				groceryList.add(I);
+				break;
+			case 2: // View Existing Items
+				I = selectItem(groceryList, s);
+				I.printDetails();
+				break;
+			case 3: // Edit an Existing Item
+				I = selectItem(groceryList, s);
+				editExistingItem(I, s);
+				break;
+			case 4: // Delete Item
+				groceryList = deleteItem(groceryList);
+				break;
+			case 5: // Quit
+				return;
+			default:
+				System.out.println("/n/nINVALID, try again.");
+				break;
+
+			}
 
 		}
 
 	}
 	
-	public static String selectNewValue(Scanner s) {
-		String newInput;
-		System.out.print("What new value would you like to replace X with? ");
-		newInput = s.nextLine();
-		newInput = s.nextLine();
-		return newInput;
-		//FIXME not allowing input
+	public static ArrayList<Item> deleteItem(ArrayList<Item> groceryList) {
+		int i = 1;
+		System.out.println("List of Grocery Items");
+		for (i = 1; i <= groceryList.size(); ++i) {
+			System.out.println("" + i + ". " + groceryList.get(i - 1).getDescription());
+		}
+
+		
+		
+		return groceryList;
 	}
 
 	public static void editExistingItem(Item I, Scanner s) {
 		String input;
 		String newValue;
-		
+
 		System.out.println();
 		I.printDetails();
-		System.out.print("Enter Property Name To Change: ");
+		System.out.print("\nEnter Property Name To Change: ");
 		input = s.nextLine();
 		input = s.nextLine();
-		
-		System.out.print("What would you like to replace it with? ");
+
+		System.out.print("\nReplace It With: ");
 		newValue = s.nextLine();
-		
-		if(input.equalsIgnoreCase("name")) {
+		System.out.println();
+
+		if (input.equalsIgnoreCase("name")) {
 			I.setDescription(newValue);
 		}
-		
-		if(input.equalsIgnoreCase("cost")) {
+
+		if (input.equalsIgnoreCase("cost")) {
 			I.setCost(Double.parseDouble(newValue));
 		}
-		
-		if(input.equalsIgnoreCase("regular price")) {
+
+		if (input.equalsIgnoreCase("regular price")) {
 			I.setRegularPrice(Double.parseDouble(newValue));
 		}
-		
-		if(input.equalsIgnoreCase("sale price")) {
+
+		if (input.equalsIgnoreCase("sale price")) {
 			I.setSalePrice(Double.parseDouble(newValue));
 		}
 
 		I.printDetails();
-		//FIXME if not numbers, will cause problems, how to fix?
-		
+		// FIXME if not numbers, will cause problems, how to fix?
+
 	}
 
 	public static Item selectItem(ArrayList<Item> groceryList, Scanner s) {
@@ -98,12 +103,10 @@ public class Prompt {
 				System.out.println("INVALID");
 				continue;
 			}
-			
-			return groceryList.get(--option);
-			
-		}
 
-		
+			return groceryList.get(--option);
+
+		}
 
 	}
 
@@ -135,22 +138,10 @@ public class Prompt {
 	public static ArrayList<Item> fillArrayList() {
 		ArrayList<Item> groceryList = new ArrayList<Item>();
 
-		String description = "Apple";
-		double cost = 0.0;
-		double regularPrice = 0.0;
-		double salePrice = 0.0;
-		double profitMargin = 0.0;
-
-		Item Apple = new Item(description, cost, regularPrice, salePrice);
+		Item Apple = new Item("Apple", 0.0, 7.0, 6.0);
 		groceryList.add(Apple);
 
-		description = "Rice";
-		cost = 0.0;
-		regularPrice = 0.0;
-		salePrice = 0.0;
-		profitMargin = 0.0;
-
-		Item Rice = new Item(description, cost, regularPrice, salePrice);
+		Item Rice = new Item("Rice", 8.0, 7.0, 5.0);
 		groceryList.add(Rice);
 
 		Item ToiletPaper = new Item("Toilet Paper", 0.55, 0.85, 0.85);
@@ -192,11 +183,12 @@ public class Prompt {
 			System.out.println("3. Edit an Existing Item");
 			System.out.println("4. Delete Item");
 			System.out.println("5. Quit");
+			System.out.print("\nEnter Number: ");
 
 			choice = s.nextInt();
 
 			if (choice > 5 || choice < 1) {
-				System.out.println("/n/nINVALID, try again.");
+				System.out.println("\n\nINVALID, try again.");
 			} else {
 				break;
 			}

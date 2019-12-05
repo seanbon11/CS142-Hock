3b3d5690f9da4919ca19c0376dd6ece31a073e69
jpheitman <58095360+jpheitman@users.noreply.github.com>
2019-12-04 20:40:19 -0800

    Updating Scraper main method

--- Final Project 142/src/Scraper.java
+++ Final Project 142/src/Scraper.java
@@ -53,16 +53,9 @@ public class Scraper {
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner s = new Scanner(new File( "brdatatest.xml" ));
-		ArrayList<String> list = new ArrayList<>();
-		while (s.hasNextLine()) {
-			String line = s.nextLine();
-			if (line.startsWith("    <Wh_Item>")) {
-				String number = line.substring(line.indexOf(">")+1, line.indexOf("</"));
-				list.add(number);
-			}
-		}
-		System.out.println(list);
-		System.out.println(list.size());
+		Scanner t = new Scanner(new File( "sampleitems.xml" ));
+		scanWarehouseCodes(s);
+		loadNewItems(t);
 	}
 
 }
